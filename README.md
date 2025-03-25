833c793f29aa0a74e68d2aa2497708bbba57bc0e
noah <number1noah@gmail.com>
2025-03-24 22:26:06 -0500

    did some setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# asciify
+Tool for converting images to ascii art
+
+-c	Colorized output
+-d	Custom dimensions. Will crop and downscale. To avoid cropping, use -s
+-s	Custom scale. Must be less than or equal to 1.
+-m	Choose mode. [edge, instensity, ...]
+-f	write to file.
--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 int main(int argc, char **argv) {
-	printf("Hello World!");
+	const char *img_name = argv[1];
+
+	int width, height;
+	unsigned char *img_data = stbi_load(img_name, &width, &height, NULL, 3);
+
+	if (img_data == NULL) {
+		printf("error\n");
+		return 1;
+	}
+	printf("no error\n");
+	printf("x:%i, y:%i\n", width, height);
+	return 0;
 }
